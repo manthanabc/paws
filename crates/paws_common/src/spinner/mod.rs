4a903920526b6f0f613d0f65fda8b706748ddaf3
Manthan <manthanmji@gmail.com>
2025-12-30 19:43:06 +0000

    fix: moved extra line handling to spinner

--- crates/paws_common/src/spinner/mod.rs
+++ crates/paws_common/src/spinner/mod.rs
@@ -102,6 +102,7 @@ impl SpinnerManager {
                         if active {
                             term(execute!(
                                 stderr,
+                                cursor::MoveUp(1),
                                 cursor::MoveToColumn(0),
                                 Clear(ClearType::CurrentLine)
                             ));
