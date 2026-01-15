sternelee/reforge

fix(operation): use cdata for warning messages to prevent html escaping (#2152)
Co-authored-by: autofix-ci[bot] <114827586+autofix-ci[bot]@users.noreply.github.com>
Co-authored-by: Tushar Mathur <tusharmath@gmail.com>

crates/forge_app/src/fmt/fmt_output.rs
@@ -142,7 +142,7 @@ mod tests {
             output: FsCreateOutput {
                 path: "/home/user/project/new_file.txt".to_string(),
                 before: None,
-                warning: None,
+                errors: vec![],
                 content_hash: crate::compute_hash(content),
             },
         };
@@ -166,7 +166,7 @@ mod tests {
             output: FsCreateOutput {
                 path: "/home/user/project/existing_file.txt".to_string(),
                 before: Some("old content".to_string()),
-                warning: None,
+                errors: vec![],
                 content_hash: crate::compute_hash(content),
             },
         };
@@ -194,7 +194,11 @@ mod tests {
             output: FsCreateOutput {
                 path: "/home/user/project/file.txt".to_string(),
                 before: None,
-                warning: Some("File created outside project directory".to_string()),
+                errors: vec![forge_domain::SyntaxError {
+                    line: 5,
+                    column: 10,
+                    message: "Syntax error".to_string(),
+                }],
                 content_hash: crate::compute_hash(content),
             },
         };
@@ -313,7 +317,7 @@ mod tests {
                 operation: PatchOperation::Replace,
             },
             output: PatchOutput {
-                warning: None,
+                errors: vec![],
                 before: "Hello world\nThis is a test".to_string(),
                 after: after_content.to_string(),
                 content_hash: crate::compute_hash(after_content),
@@ -336,7 +340,11 @@ mod tests {
                 operation: PatchOperation::Replace,
             },
             output: PatchOutput {
-                warning: Some("Large file modification".to_string()),
+                errors: vec![forge_domain::SyntaxError {
+                    line: 10,
+                    column: 5,
+                    message: "Syntax error".to_string(),
+                }],
                 before: "line1\nline2".to_string(),
                 after: after_content.to_string(),
                 content_hash: crate::compute_hash(after_content),

manthanabc/paws

crates/paws_app/src/fmt/fmt_output.rs
@@ -146,6 +146,7 @@ mod tests {
             output: FsCreateOutput {
                 path: "/home/user/project/new_file.txt".to_string(),
                 before: None,
+                errors: vec![],
 
                 content_hash: crate::compute_hash(content),
             },
@@ -170,6 +171,7 @@ mod tests {
             output: FsCreateOutput {
                 path: "/home/user/project/existing_file.txt".to_string(),
                 before: Some("old content".to_string()),
+                errors: vec![],
 
                 content_hash: crate::compute_hash(content),
             },
@@ -198,6 +200,11 @@ mod tests {
             output: FsCreateOutput {
                 path: "/home/user/project/file.txt".to_string(),
                 before: None,
+                errors: vec![paws_domain::SyntaxError {
+                    line: 5,
+                    column: 10,
+                    message: "Syntax error".to_string(),
+                }],
 
                 content_hash: crate::compute_hash(content),
             },
@@ -317,6 +324,8 @@ mod tests {
                 operation: PatchOperation::Replace,
             },
             output: PatchOutput {
+                errors: vec![],
+
                 before: "Hello world\nThis is a test".to_string(),
                 after: after_content.to_string(),
                 content_hash: crate::compute_hash(after_content),
@@ -339,6 +348,12 @@ mod tests {
                 operation: PatchOperation::Replace,
             },
             output: PatchOutput {
+                errors: vec![paws_domain::SyntaxError {
+                    line: 10,
+                    column: 5,
+                    message: "Syntax error".to_string(),
+                }],
+
                 before: "line1\nline2".to_string(),
                 after: after_content.to_string(),
                 content_hash: crate::compute_hash(after_content),