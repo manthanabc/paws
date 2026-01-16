sternelee/reforge

fix(auth): prefill existing api key in prompt (#2138)
Co-authored-by: autofix-ci[bot] <114827586+autofix-ci[bot]@users.noreply.github.com>
Co-authored-by: Tushar Mathur <tusharmath@gmail.com>

crates/forge_main/src/ui.rs
@@ -1814,11 +1814,17 @@ impl<A: API + 'static, F: Fn() -> A + Send + Sync> UI<A, F> {
             })
             .collect::<anyhow::Result<HashMap<_, _>>>()?;
 
-        let api_key = ForgeSelect::input(format!("Enter your {provider_id} API key:"))
-            .prompt()?
-            .context("API key input cancelled")?;
+        let input = if let Some(default_key) = &request.api_key {
+            // ApiKey's Display shows masked version, AsRef<str> gives actual value
+            ForgeSelect::input(format!("Enter your {provider_id} API key:"))
+                .with_default(default_key)
+        } else {
+            ForgeSelect::input(format!("Enter your {provider_id} API key:"))
+        };
 
-        let api_key_str = api_key.trim();
+        let api_key_str = input.prompt()?.context("API key input cancelled")?;
+
+        let api_key_str = api_key_str.trim();
         anyhow::ensure!(!api_key_str.is_empty(), "API key cannot be empty");
 
         // Update the context with collected data

manthanabc/paws

crates/paws_main/src/ui.rs
@@ -1833,11 +1833,17 @@ impl<A: API + 'static, F: Fn() -> A + Send + Sync> UI<A, F> {
             })
             .collect::<anyhow::Result<HashMap<_, _>>>()?;
 
-        let api_key = PawsSelect::input(format!("Enter your {provider_id} API key:"))
-            .prompt()?
-            .context("API key input cancelled")?;
+        let input = if let Some(default_key) = &request.api_key {
+            // ApiKey's Display shows masked version, AsRef<str> gives actual value
+            PawsSelect::input(format!("Enter your {provider_id} API key:"))
+                .with_default(default_key)
+        } else {
+            PawsSelect::input(format!("Enter your {provider_id} API key:"))
+        };
 
-        let api_key_str = api_key.trim();
+        let api_key_str = input.prompt()?.context("API key input cancelled")?;
+
+        let api_key_str = api_key_str.trim();
         anyhow::ensure!(!api_key_str.is_empty(), "API key cannot be empty");
 
         // Update the context with collected data