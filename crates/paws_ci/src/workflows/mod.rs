77cf954882abcb29a5f39f042c3e447e1077eb39
Manthan <manthanmji@gmail.com>
2026-01-24 22:13:39 +0000

    refactor: drop stale issue workflow

--- crates/paws_ci/src/workflows/mod.rs
+++ crates/paws_ci/src/workflows/mod.rs
@@ -5,11 +5,9 @@ mod ci;
 mod labels;
 mod release_drafter;
 mod release_publish;
-mod stale;
 
 pub use autofix::*;
 pub use ci::*;
 pub use labels::*;
 pub use release_drafter::*;
 pub use release_publish::*;
-pub use stale::*;
